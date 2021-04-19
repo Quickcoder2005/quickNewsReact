e21c377b98931e42cde08d707f54c703a6e563d9
QuickCoder <72077110+Quickcoder2005@users.noreply.github.com>
2021-04-19 18:15:07 +0530

    Update AboutContainer.js

--- src/AboutContainer.js
+++ src/AboutContainer.js
@@ -1,10 +1,1 @@
-import React from "react";
-import {Link, Typography} from "@material-ui/core";
 
-function AboutContainer(){
-	return (
-		<Typography style={{marginTop: "80px", marginLeft: "50px", marginRight: "50px"}} variant="h6" color="textSecondary" align="center"paragraph> This web app QuickNews is made by QuickCoder as an experiment project. This web app makes news available in shorter form to people who have less time or want to save their time. Make sure to check the project <Link href={"https://github.com/Quickcoder2005/quickNewsReact"} target="_blank" rel="noopener noreferrer"> here</Link>. </Typography>
-	);
-}
-
-export default AboutContainer;
